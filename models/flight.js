28274792d322a7d62fabb1e905fbbc72218d62c8
Drew Neece <dneece5@aggienetwork.com>
2023-01-10 16:47:43 -0600

    update model properties (step 3)

--- models/flight.js
+++ models/flight.js
@@ -4,10 +4,30 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const flightSchema = new Schema({
-  airline: String,
-  airport: {type: String, default: 'DFW'},
-  flightNo: Number,
-  departs: Date
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United']
+  },
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+    default: 'DFW'
+  },
+  flightNo: {
+    type: Number,
+    min: 10,
+    max: 9999
+  },
+  departs: {
+    type: Date, 
+    default: function() {
+      const today = new Date()
+      console.log(today)
+      const oneYearLater = today.getFullYear() + 1
+      console.log(oneYearLater)
+      return oneYearLater
+    }
+  }
 }, {
   timestamps: true
 })
