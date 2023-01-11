3a1dbfaef40bb6a3dca0d41881dd4510152d3787
Drew Neece <dneece5@aggienetwork.com>
2023-01-10 22:16:27 -0600

    add delete function that allows user to delete flights

--- routes/flights.js
+++ routes/flights.js
@@ -15,6 +15,9 @@ router.get('/:id', flightsCtrl.show)
 // POST localhost:3000/flights
 router.post('/', flightsCtrl.create)
 
+// DELETE localhose:3000/flights/:id
+router.delete('/:id', flightsCtrl.delete)
+
 export {
   router
 }
